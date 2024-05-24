315f9fb76f7fd166377779bcb90cdd7563c0c137
Marlon Couto <ozair.couto@trieduc.com.br>
2024-05-24 13:18:10 -0300

    Atualiza implementação

--- Normalize/Program.cs
+++ Normalize/Program.cs
@@ -4,19 +4,35 @@ namespace Normalize;
 
 internal class Program
 {
+    private static readonly string HomeDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
     private static string _path = Directory.GetCurrentDirectory();
 
     private static void Main(string[] args)
     {
+        string? pathArg = null;
         if (args.Length == 1)
         {
-            _path = Path.GetFullPath(args[0]);
+            pathArg = args[0];
+        }
+
+        if (pathArg != null)
+        {
+            if (pathArg.StartsWith("~/") || pathArg.StartsWith("~\\"))
+            {
+                _path = pathArg.Replace("~", HomeDir).Replace('/', Path.DirectorySeparatorChar);
+            }
+            else
+            {
+                _path = Path.GetFullPath(pathArg);
+            }
         }
 
         var files = Directory.GetFiles(_path);
         foreach (var file in files)
         {
-            FileHelpers.NormalizeFilenameOrPath(file);
+            var normalizedFilename = FileHelpers.NormalizeFilenameOrPath(Path.GetFileNameWithoutExtension(file));
+            var newFilename = $"{normalizedFilename}{Path.GetExtension(file)}";
+            File.Move(file, Path.Combine(_path, newFilename));
         }
 
         Console.WriteLine("Tarefas concluídas.");
