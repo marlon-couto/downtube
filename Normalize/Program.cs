8fa0a05fc531a0d3e99a2b6e9eb4d24c4277c4cd
Marlon Couto <ozair.couto@trieduc.com.br>
2024-05-24 10:44:19 -0300

    Cria o projeto de console Normalize

---
+++ Normalize/Program.cs
@@ -0,0 +1,24 @@
+﻿using DownTube.Core.Helpers;
+
+namespace Normalize;
+
+internal class Program
+{
+    private static string _path = Directory.GetCurrentDirectory();
+
+    private static void Main(string[] args)
+    {
+        if (args.Length == 1)
+        {
+            _path = Path.GetFullPath(args[0]);
+        }
+
+        var files = Directory.GetFiles(_path);
+        foreach (var file in files)
+        {
+            FileHelpers.NormalizeFilenameOrPath(file);
+        }
+
+        Console.WriteLine("Tarefas concluídas.");
+    }
+}
